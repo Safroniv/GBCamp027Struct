687cb3ec44e27faa3f2dc80803298d6e77010d79
Иван Сафронов <safroniv@mail.ru>
2022-07-21 22:06:34 +0300

    My small crm fin

--- MySmallCRM/Program.cs
+++ MySmallCRM/Program.cs
@@ -1,10 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using MySmallCRM.Models;
+using MySmallCRM.Repositories;
+using MySmallCRM.Services;
+
+Console.WriteLine("MySmallPerson");
+PersonRepository personRepository = new PersonRepository();
+PersonService personService = new PersonService(personRepository);
+
+personService.CreateNewPerson("Konstantin", "Vaskin");
+
+if (personService.CreateNewPerson("Xasiliy", "Pupopkin") == true)
+{
+    List<Person> persons = personService.GetAllPresons();
+    foreach (Person person in persons)
+    {
+        Console.WriteLine(person.Id + " " + person.Name + " " + person.SurName );
+    }
+}
+
+
 
 /*
  * Model
  * Service
- * Datasource
+ * Repositories ->CRUD Create Read Update Delete
  * 
  */
 
---
+++ MySmallCRM/Services/PersonService.cs
@@ -0,0 +1,44 @@
+﻿using MySmallCRM.Models;
+using MySmallCRM.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySmallCRM.Services
+{
+    //private readonly PersonRepository _repository;
+    public class PersonService
+    {
+        private PersonRepository _repository;
+
+        public PersonService (PersonRepository repository)
+        {
+            _repository = repository;
+        }
+        public bool CreateNewPerson(string name, string surname)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(surname))
+            {
+                return false;
+            }
+
+            Person person = new Person();
+            person.Name = name;
+            person.SurName = surname;
+
+            _repository.Create(person);
+            return true;
+        }
+        public List<Person> GetAllPresons()
+        {
+            return _repository.GetAll();
+        }
+    }
+
+}
